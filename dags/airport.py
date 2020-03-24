d12a3eeb142bab194aacb0b6b4833cc36221b255
chetai <tai.chester@gmail.com>
2020-03-24 15:19:03 -0700

    airport dag update
    added auto load from csv in gcs

--- dags/airport.py
+++ dags/airport.py
@@ -3,11 +3,12 @@ from datetime import datetime, timedelta
 import airflow
 from airflow import DAG
 from airflow import configuration as conf
+from airflow import models
 from airflow.utils.helpers import chain
 from airflow.operators.bash_operator import BashOperator
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.contrib.operators.bigquery_operator import BigQueryOperator
-
+from airflow.contrib.operators.gcs_to_bq import GoogleCloudStorageToBigQueryOperator
 
 default_args = {
     'owner': 'Chester Tai',
@@ -18,10 +19,12 @@ default_args = {
     'retries': 2,
     'retry_delay': timedelta(minutes=5),
 }
-pacific_time_three_am = '0 12 * * *'
+
+#pipeline schedule
+pacific_time_three_am = '0 11 * * *'
 
 dag = DAG(
-    'airport',
+    'airport_auto',
     default_args=default_args,
     schedule_interval=pacific_time_three_am
 )
@@ -30,16 +33,28 @@ BQ_CONN_ID = "my_gcp_conn"
 BQ_PROJECT = "practice-271500"
 BQ_DATASET = "sandbox"
 
-
 with dag:
+
+    #load airport csv from gcs to bq airport_auto
+    load_airport_csv = GoogleCloudStorageToBigQueryOperator(
+        task_id='airport_gcs_to_bq',
+        bucket='us-central1-slalom-practice-eef43966-bucket',
+        source_objects=['data/raw_data/airport-codes_csv.csv'],
+        destination_project_dataset_table='sandbox.airport_auto',
+        source_format='CSV',
+        create_disposition='CREATE_IF_NEEDED',
+        skip_leading_rows=1,        
+        write_disposition='WRITE_TRUNCATE',
+        autodetect=True
+    )
+
+    #DDL for airport_na_auto
     airport_na_create = BigQueryOperator(
-        task_id='airport_na_create',
+        task_id='airport_na_auto_create',
         sql='''
         #standardSQL
-        DROP TABLE IF EXISTS
-        `practice-271500.sandbox.airport_na`;
         CREATE TABLE IF NOT EXISTS
-        `practice-271500.sandbox.airport_na`(
+        `practice-271500.sandbox.airport_na_auto`(
             ident STRING,
             type STRING,
             name STRING,
@@ -57,13 +72,14 @@ with dag:
         bigquery_con_id=BQ_CONN_ID
     )
 
+    # illustration - creating a derivative table from an existing BQ table. in the real world we might do this directly from the csv, depending on cost effectiveness.
     airport_na_insert = BigQueryOperator(
-        task_id='airport_na_insert',
+        task_id='airport_na_auto_insert',
         sql='''
         #standardSQL
-        DELETE FROM sandbox.airport_na WHERE TRUE;
-        INSERT INTO sandbox.airport_na
-        SELECT * FROM sandbox.airport WHERE
+        DELETE FROM sandbox.airport_na_auto WHERE TRUE;
+        INSERT INTO sandbox.airport_na_auto
+        SELECT * FROM sandbox.airport_auto WHERE
             type != 'closed'
             AND continent = 'NA';
         ''',
@@ -71,6 +87,9 @@ with dag:
         bigquery_conn_id=BQ_CONN_ID       
     )
 
-chain(airport_na_create, airport_na_insert)
+    load_airport_csv >> airport_na_insert
+    airport_na_create >> airport_na_insert
+
+    #linear chaining(not as efficient): chain(load_airport_csv, airport_na_create, airport_na_insert)
 
     
